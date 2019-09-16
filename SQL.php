c0ee681dce01fbc8ce5409fee4530ad52989e2da
francoZuniga32 <franco.ojeda@est.fi.uncoma.edu.ar>
2019-09-16 18:57:10 -0300

    d1609h1857

--- SQL.php
+++ SQL.php
@@ -2,12 +2,16 @@
 require_once("connecion.php");
 require_once("consulta.php");
 
-$sql = "SELECT * FROM `imparte`, `materia` WHERE materia.idMateria = imparte.idMateria AND imparte.idCarrera = '3' AND materia.ano = '4'";
+$sql = "SELECT * FROM `imparte`, `materia` WHERE materia.idMateria = imparte.idMateria AND imparte.idCarrera = '2' AND materia.ano = '4'";
 $consulta = new CONSULTA();
 $respuestaMateriasTUASySL = $consulta->getConsulta($sql);
 //armamos las distintas consultas TUASySL
 
 foreach($respuestaMateriasTUASySL as $materia){
     echo "new Array(\"".$materia['nombre']." - ".$materia['cuatrimestre']."\",\"".$materia['idMateria']."\"),</br>"; 
+}
+
+foreach($respuestaMateriasTUASySL as $materia){
+
 }
 ?>
