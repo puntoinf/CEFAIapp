471886bef9145b328b89532092e23eabf2062700
franco <franco.ojeda@est.fi.uncoma.edu.ar>
2019-11-25 16:23:47 -0300

    d2511h1623

--- administracion/pdf.php
+++ administracion/pdf.php
@@ -1,23 +1,25 @@
-<style>
-
-
-</style>
-
 <?php 
 	require '../vendor/autoload.php';
-
-    use Spipu\Html2Pdf\Html2Pdf;
     /**
      * recogemos el contenido de una estructura php
      */
-    
-    ob_start();
-    require_once 'estructuraPdfCorrelativas.php';
-    $html = ob_get_clean();
 
-    
+    use Spipu\Html2Pdf\Html2Pdf;
+    use Spipu\Html2Pdf\Exception\Html2PdfException;
+    use Spipu\Html2Pdf\Exception\ExceptionFormatter;
 
-    $html2pdf = new Html2Pdf('P', 'A4', 'en', true, 'UTF-8');
-    $html2pdf->WriteHTML($html);
-    $html2pdf->output();
+    try {
+        ob_start();
+        require_once 'estructuraPdfCorrelativas.php';
+        $content = ob_get_clean();
+
+        $html2pdf = new Html2Pdf('P', 'A4', 'es');
+        $html2pdf->writeHTML($content);
+        $html2pdf->output('estructuraPdfCorrelativas.pdf');
+    } catch (Html2PdfException $e) {
+        $html2pdf->clean();
+
+        $formatter = new ExceptionFormatter($e);
+        echo $formatter->getHtmlMessage();
+    }
 ?>
